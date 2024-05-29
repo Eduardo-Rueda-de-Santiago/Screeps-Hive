77109c48dcc268365a0c3521871bf5ec061ebd37
Eduardo Rueda de Santiago <eduardoruedadesantiago@gmail.com>
2024-05-29 15:57:56 +0200

    a

--- src/main.ts
+++ src/main.ts
@@ -34,6 +34,8 @@ declare global {
 export const loop = ErrorMapper.wrapLoop(() => {
   console.log(`Current game tick is ${Game.time}`);
 
+  Game.spawns[0].spawnCreep([MOVE], "Duck" );
+
   // Automatically delete memory of missing creeps
   for (const name in Memory.creeps) {
     if (!(name in Game.creeps)) {
