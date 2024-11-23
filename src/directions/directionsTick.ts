91123ddf39726cbaac7de4f9c9c146e0f1a32b90
Eduardo Rueda de Santiago <eduardoruedadesantiago@gmail.com>
2024-11-23 20:06:59 +0100

    Added example types

--- src/directions/directionsTick.ts
+++ src/directions/directionsTick.ts
@@ -1,6 +1,5 @@
 import { CreepsReport } from "../creeps/creepsTick";
 import { StructuresReport } from "../structures/structuresTick";
-import * as console from "console";
 
 export function directionsTick(tick: number, creepsReport: CreepsReport, structuresReport: StructuresReport) {
   console.log("Doing directions tick");
